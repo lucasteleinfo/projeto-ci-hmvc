f60515e2463d5985735822dbd338812fe385aed7
Lucas França <lucas.teleinfo@gmail.com>
2016-03-15 01:13:13 -0300

    Primeiro commit

---
+++ application/modules/home/views/v_home.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="description" content="Projeto Padrão CodeIgniter HMVC">
+    <meta name="keywords" content="CodeIgniter,HMVC">
+    <meta name="author" content="Lucas França">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Projeto Padrão CodeIgniter HMVC</title>
+</head>
+<body>
+    <h1>Projeto Padrão CodeIgniter HMVC</h1>
+</body>
+</html>
